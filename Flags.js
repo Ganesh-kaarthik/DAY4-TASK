6d3f326fe988cb8092a72c57cfc279967081c221
Ganesh-kaarthik <110884217+Ganesh-kaarthik@users.noreply.github.com>
2022-08-18 00:07:13 +0530

    Add files via upload

---
+++ Flags.js
@@ -0,0 +1,28 @@
+//make a http request
+
+//create an instance of the request 
+let xhr=new XMLHttpRequest();
+
+//Open your request 
+xhr.open("GET","https://restcountries.com/v2/all");
+
+//onload event 
+xhr.onload=function(){
+    console.log("************Response Received************");
+    console.log(this.responseText);
+    let data=JSON.parse(xhr.responseText)
+    console.log(data)
+    
+    
+//Loop throigh the data and print it on the console
+for(let i=0;i<data.length;i++){
+   console.log(`
+   Country: ${data[i].name}; 
+   Flag: ${data[i].flags.svg};
+   `)
+}
+    
+}
+
+//send the request
+xhr.send();
